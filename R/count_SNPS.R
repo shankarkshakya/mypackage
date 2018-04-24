55d30e3d18275562e76e746a1cfa71f0f11a796b
Shankar Kaji Shakya <shankarkshakya@users.noreply.github.com>
2018-04-24 15:46:30 -0700

    Update count_SNPS.R

--- R/count_SNPS.R
+++ R/count_SNPS.R
@@ -18,7 +18,7 @@
 count_SNPS <- function(dnabin) {
 
   x <- mypackage::seg.sites2codons(dnabin)
-  x <- x[unique(rownames(x)),]
+  x <- x[unique(rownames(x)), ,drop = FALSE ]
 
   if (length(x != 0 )){
     seg_mat <- vector("list", length = nrow(x))
