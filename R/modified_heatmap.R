5ebc5d3477fc5643640701d4dabeadbfa6674a65
Shankar Shakya <12001003498917@fedidcard.gov>
2019-03-11 10:00:26 -0700

    documentation heatmap

--- R/modified_heatmap.R
+++ R/modified_heatmap.R
@@ -1,12 +1,23 @@
-modified_heatmap <- function (x, cbarplot = TRUE, rbarplot = TRUE, legend = TRUE, 
-                  clabels = TRUE, rlabels = TRUE, na.rm = TRUE, scale = c("row", 
-                                                                          "column", "none"), col.ramp = viridisLite::viridis(n = 100, 
-                                                                                                                             alpha = 1), ...) 
+#' @title Heatmap to modify vcfR::heatmap.bp.
+#' @description Removes bargraph from axes.
+#' @param matrix.
+#' @return image
+#'
+#' @details
+#' Remove bargraph from axes.
+#'
+#'
+#' @export
+
+modified_heatmap <- function (x, cbarplot = TRUE, rbarplot = TRUE, legend = TRUE,
+                  clabels = TRUE, rlabels = TRUE, na.rm = TRUE, scale = c("row",
+                                                                          "column", "none"), col.ramp = viridisLite::viridis(n = 100,
+                                                                                                                             alpha = 1), ...)
 {
-  
-  
+
+
   stopifnot(class(x) == "matrix")
-  scale <- if (missing(scale)) 
+  scale <- if (missing(scale))
     "none"
   else match.arg(scale)
   nrows <- 1
@@ -43,49 +54,49 @@ modified_heatmap <- function (x, cbarplot = TRUE, rbarplot = TRUE, legend = TRUE
   if (cbarplot == FALSE & rbarplot == FALSE & legend == FALSE) {
   }
   if (cbarplot == FALSE & rbarplot == TRUE & legend == FALSE) {
-    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols, 
+    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols,
                             byrow = TRUE), widths = c(4, 0.6))
   }
   if (cbarplot == FALSE & rbarplot == FALSE & legend == TRUE) {
-    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols, 
+    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols,
                             byrow = TRUE), widths = c(4, 0.2))
   }
   if (cbarplot == FALSE & rbarplot == TRUE & legend == TRUE) {
-    graphics::layout(matrix(1:3, nrow = nrows, ncol = ncols, 
+    graphics::layout(matrix(1:3, nrow = nrows, ncol = ncols,
                             byrow = TRUE), widths = c(4, 0.6, 0.2))
   }
   if (cbarplot == TRUE & rbarplot == FALSE & legend == FALSE) {
-    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols, 
+    graphics::layout(matrix(1:2, nrow = nrows, ncol = ncols,
                             byrow = TRUE), heights = c(1, 4))
   }
   if (cbarplot == TRUE & rbarplot == TRUE & legend == FALSE) {
-    graphics::layout(matrix(1:4, nrow = nrows, ncol = ncols, 
-                            byrow = TRUE), widths = c(4, 0.6), heights = c(1, 
+    graphics::layout(matrix(1:4, nrow = nrows, ncol = ncols,
+                            byrow = TRUE), widths = c(4, 0.6), heights = c(1,
                                                                            4))
   }
   if (cbarplot == TRUE & rbarplot == FALSE & legend == TRUE) {
-    graphics::layout(matrix(1:4, nrow = nrows, ncol = ncols, 
-                            byrow = TRUE), widths = c(4, 0.2), heights = c(1, 
+    graphics::layout(matrix(1:4, nrow = nrows, ncol = ncols,
+                            byrow = TRUE), widths = c(4, 0.2), heights = c(1,
                                                                            4))
   }
   if (cbarplot == TRUE & rbarplot == TRUE & legend == TRUE) {
-    graphics::layout(matrix(1:6, nrow = nrows, ncol = ncols, 
-                            byrow = TRUE), widths = c(4, 0.6, 0.2), heights = c(1, 
+    graphics::layout(matrix(1:6, nrow = nrows, ncol = ncols,
+                            byrow = TRUE), widths = c(4, 0.6, 0.2), heights = c(1,
                                                                                 4))
   }
   graphics::par(mar = c(0, 0, 0, 0))
   graphics::par(oma = c(1, 1, 1, 1))
   if (cbarplot == TRUE) {
-    graphics::barplot(colSums(x, na.rm = na.rm), space = 0, 
-                      border = NA, axes = FALSE, names.arg = "", col = c("#FFFFFF", 
+    graphics::barplot(colSums(x, na.rm = na.rm), space = 0,
+                      border = NA, axes = FALSE, names.arg = "", col = c("#FFFFFF",
                                                                          "#FFFFFF"), xaxs = "i")
     if (clabels == TRUE & scale == "none") {
-      graphics::text(c(1:ncol(x)) - 0.5, 0, colnames(x), 
+      graphics::text(c(1:ncol(x)) - 0.5, 0, colnames(x),
                      adj = c(0, 0.5), srt = 90)
     }
     else if (clabels == TRUE & scale != "none") {
-      graphics::text(c(1:ncol(x)) - 0.5, min(colSums(x, 
-                                                     na.rm = na.rm), na.rm = na.rm), colnames(x), 
+      graphics::text(c(1:ncol(x)) - 0.5, min(colSums(x,
+                                                     na.rm = na.rm), na.rm = na.rm), colnames(x),
                      adj = c(0, 0.5), srt = 90)
     }
     if (rbarplot == TRUE) {
@@ -97,27 +108,27 @@ modified_heatmap <- function (x, cbarplot = TRUE, rbarplot = TRUE, legend = TRUE
   }
   graphics::image(t(x), col = col.ramp, axes = FALSE, frame.plot = TRUE)
   if (rbarplot == TRUE) {
-    graphics::barplot(rowSums(x, na.rm = na.rm), space = 0, 
-                      border = NA, horiz = TRUE, axes = FALSE, names.arg = "", 
+    graphics::barplot(rowSums(x, na.rm = na.rm), space = 0,
+                      border = NA, horiz = TRUE, axes = FALSE, names.arg = "",
                       col = c("#FFFFFF", "#FFFFFF"), yaxs = "i")
     if (rlabels == TRUE & scale == "none") {
-      graphics::text(0, c(1:nrow(x)) - 0.5, rownames(x), 
+      graphics::text(0, c(1:nrow(x)) - 0.5, rownames(x),
                      adj = c(0, 0.5), srt = 0)
     }
     else if (rlabels == TRUE & scale != "none") {
-      graphics::text(min(rowSums(x, na.rm = na.rm), na.rm = na.rm), 
-                     c(1:nrow(x)) - 0.5, rownames(x), adj = c(0, 0.5), 
+      graphics::text(min(rowSums(x, na.rm = na.rm), na.rm = na.rm),
+                     c(1:nrow(x)) - 0.5, rownames(x), adj = c(0, 0.5),
                      srt = 0)
     }
   }
   if (legend == TRUE) {
-    mp <- graphics::barplot(rep(1, times = length(col.ramp)), 
-                            space = 0, border = NA, horiz = TRUE, col = col.ramp, 
+    mp <- graphics::barplot(rep(1, times = length(col.ramp)),
+                            space = 0, border = NA, horiz = TRUE, col = col.ramp,
                             axes = FALSE)
     if (mp[nrow(mp), 1] - mp[1, 1] >= 1) {
-      graphics::text(0.5, mp[1, 1], "Low", col = "#FFFFFF", 
+      graphics::text(0.5, mp[1, 1], "Low", col = "#FFFFFF",
                      adj = c(0.5, 0))
-      graphics::text(0.5, mp[nrow(mp), 1], "High", col = "#FFFFFF", 
+      graphics::text(0.5, mp[nrow(mp), 1], "High", col = "#FFFFFF",
                      adj = c(0.5, 1))
     }
   }
